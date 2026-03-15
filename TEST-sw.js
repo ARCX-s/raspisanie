557f2b8e6e5938c4f07b1a30143288ba6e11fe7b
ARCX-s <gsoft8161@gmail.com>
2026-03-15 19:05:07 +0300

    Update TEST-sw.js

--- TEST-sw.js
+++ TEST-sw.js
@@ -1,19 +1,19 @@
-const CACHE = 'sched-v8';
-const FILES = ['./app.js', './data.js', './manifest.json'];
+//const CACHE = 'sched-v8';
+//const FILES = ['./app.js', './data.js', './manifest.json'];
 
-self.addEventListener('install', e => {
+//self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
+//self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
+//self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
   );
